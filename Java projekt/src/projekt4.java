a22bcf2a8845f215139a3b9e416f92a24220527b
TE22A-Haukebo-Meo <meo.haukebo@elev.ga.ntig.se>
2023-09-04 09:14:31 +0200

    Multiplik.

--- Java projekt/src/projekt4.java
+++ Java projekt/src/projekt4.java
@@ -7,5 +7,9 @@ public class projekt4 {
         int produkt;
         summa = tal1+tal2;
         System.out.println(tal1+"+"+tal2+"="+summa);
+        int tal3 = 12345;
+        int tal4 = 6789;
+        produkt = tal2*tal3*tal4;
+        System.out.println(tal2+"*"+tal3+"*"+tal4+"="+produkt);
     }
 }
