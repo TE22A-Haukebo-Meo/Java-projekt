19d1f6f3f7c0c341b763d589aa60b5e7b16075a1
TE22A-Haukebo-Meo <meo.haukebo@elev.ga.ntig.se>
2023-09-04 09:17:48 +0200

    division

--- Java projekt/src/projekt4.java
+++ Java projekt/src/projekt4.java
@@ -2,7 +2,7 @@ public class projekt4 {
 
     public static void main(String[] args){
         int tal1 = 19;
-        int tal2 = 31;
+        int tal2 = 32;
         int summa;
         int produkt;
         summa = tal1+tal2;
@@ -11,5 +11,7 @@ public class projekt4 {
         int tal4 = 6789;
         produkt = tal2*tal3*tal4;
         System.out.println(tal2+"*"+tal3+"*"+tal4+"="+produkt);
+        int kvot = tal3/tal2;
+        System.out.println(tal3+"/"+tal2+"="+kvot);
     }
 }
