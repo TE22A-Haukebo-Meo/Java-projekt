2dbe2e5443de2b657179c1c493a966aaebe063c7
TE22A-Haukebo-Meo <meo.haukebo@elev.ga.ntig.se>
2023-09-04 09:10:46 +0200

    Addition

---
+++ Java projekt/src/projekt4.java
@@ -0,0 +1,11 @@
+public class projekt4 {
+
+    public static void main(String[] args){
+        int tal1 = 19;
+        int tal2 = 31;
+        int summa;
+        int produkt;
+        summa = tal1+tal2;
+        System.out.println(tal1+"+"+tal2+"="+summa);
+    }
+}
