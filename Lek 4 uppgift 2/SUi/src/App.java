775fd743b9c7ff5a6889fdbfc2a3a26083c695dc
TE22A-Haukebo-Meo <meo.haukebo@elev.ga.ntig.se>
2023-09-11 09:23:26 +0200

    String test

---
+++ Lek 4 uppgift 2/SUi/src/App.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner tb = new Scanner(System.in);
+        System.out.println("Droppa en cool mening");
+        String cool_mening = tb.next();
+    }
+}
