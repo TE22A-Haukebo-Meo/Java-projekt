2cd1a014257a72bd51d9f3a864cdcb0e008db6f0
TE22A-Haukebo-Meo <meo.haukebo@elev.ga.ntig.se>
2023-09-11 08:54:25 +0200

    Typomvandling

--- Lek 4 Uppgift 1.java/src/App.java
+++ Lek 4 Uppgift 1.java/src/App.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        Scanner tb = new Scanner(System.in);
+        System.out.println("Första talet?");
+        int tal1 = tb.nextInt();
+        System.out.println("Andra talet?");
+        int tal2 = tb.nextInt();
+            tb.nextLine();
+        double svar = (double)tal1/tal2;
+        System.out.println(tal1+"/"+tal2+"="+svar);
     }
 }
