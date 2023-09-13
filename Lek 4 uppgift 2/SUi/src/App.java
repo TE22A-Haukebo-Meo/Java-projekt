d353b626157b85bf0298f2e2fc07c1bd4e2cef19
TE22A-Haukebo-Meo <meo.haukebo@elev.ga.ntig.se>
2023-09-13 10:07:19 +0200

    Gjort uppgift 2b

--- Lek 4 uppgift 2/SUi/src/App.java
+++ Lek 4 uppgift 2/SUi/src/App.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner tb = new Scanner(System.in);
-        System.out.println("Droppa en cool mening");
-        String cool_mening = tb.next();
+        System.out.println("Droppa en cool mening, glöm inte att avsluta med en punkt!");
+        String cool_mening = tb.nextLine();
+        int antal_bokstäver = cool_mening.length();
+        System.out.println("Vilken bokstav ska programmet hitta?");
+        String bokstav = tb.nextLine();
+        int plats = cool_mening.indexOf(bokstav);
+        System.out.println("din mening innehåller "+antal_bokstäver+" bokstäver.");
+        System.out.println("Bokstaven "+bokstav+" ligger på platsen "+plats+" suiii");
     }
 }
